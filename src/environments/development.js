bb91c98b23b6217aee0819d456660fe909f78741
Guilherme Patriarca <gpatriarca@ubiwhere.com>
2021-04-21 08:18:37 +0100

    feat: add endpoint

--- src/environments/development.js
+++ src/environments/development.js
@@ -1,3 +1,4 @@
 export const API_MARKET_PLACE = 'http://10.30.8.55:8080/tmf-api'
 export const API_GOVERNANCE = 'https://5gzorro.alticelabs.com'
+export const API_SMART_CONTRACT = ''
 export const APP_ENV = 'development'
