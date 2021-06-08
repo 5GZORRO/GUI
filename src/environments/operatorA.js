c9b72dcc3e66c948ab64e20bb7a5391d420557dc
Rogério Silva <rsilva@ubiwhere.com>
2021-06-08 18:44:08 +0100

    fix(env): changed marketplace endpoints

--- src/environments/operatorA.js
+++ src/environments/operatorA.js
@@ -1,4 +1,4 @@
-export const API_MARKET_PLACE = 'http://172.28.3.30:8080/tmf-api'
+export const API_MARKET_PLACE = 'http://172.28.3.126:31080/tmf-api'
 export const API_LPT = 'http://172.28.3.111/operatora/legal-prose-repository'
 export const API_GOVERNANCE = 'http://172.28.3.111/operatora/governance'
 export const API_IPM = 'http://172.28.3.153:6200'
