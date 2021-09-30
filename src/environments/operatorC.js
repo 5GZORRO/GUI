425d398ca20cfcee2571e09bffcc3e47603ba19f
Rogério Silva <rsilva@ubiwhere.com>
2021-09-30 09:25:05 +0100

    feat(deploy): add operator C

--- src/environments/operatorC.js
+++ src/environments/operatorC.js
@@ -1,14 +1,15 @@
 export const API_MARKET_PLACE = 'http://172.28.3.30:8080/tmf-api'
-export const API_LPT = 'http://172.28.3.111/operatorc/legal-prose-repository'
-export const API_GOVERNANCE = 'http://172.28.3.111/operatorc/governance'
-export const API_IPM = 'http://172.28.3.153:6800'
-export const API_SMART_CONTRACT = 'http://172.28.3.111/operatorc/api/v1'
+export const API_LPT = 'http://172.28.3.111/operatora/legal-prose-repository'
+export const API_GOVERNANCE = 'http://172.28.3.111/operatora/governance'
+export const API_IPM = 'http://172.28.3.153:6600'
+export const API_SMART_CONTRACT = 'http://172.28.3.111/operatorc/smart-contract-lifecycle-manager/'
 
 export const XRM_ENDPOINT = 'http://172.28.3.126:31081'
 export const XRM_DISCOVERY_API_KEY = '61a93c48-992d-4f4f-a8c9-2433b090d54b'
 export const XRM_TRANSLATOR_API_KEY = 'bece6398-437e-4a30-9c41-2afc8388e7bd'
 export const RAPP_DISCOVERY_API_KEY = '7fb384dc-12aa-4a23-af93-74c8f7dd2e67'
 
-export const APP_ENV = 'production'
-export const VERIFICATION_KEY = 'ECBkoZd4J9VtbNhNL7FVr5ACnq6XGQKksXHRAYCN1ZB1'
-export const LEDGER_IDENTITY = 'CN=OperatorA,OU=DLT,O=DLT,L=London,C=GB'
+export const APP_ENV = 'development'
+
+export const VERIFICATION_KEY = '8mvViDZLy27uTotxokSUeY6xoR7zfKi4JpEVjLQFGEeX'
+export const LEDGER_IDENTITY = 'O=OperatorC,OU=Barcelona,L=Barcelona,C=ES'
