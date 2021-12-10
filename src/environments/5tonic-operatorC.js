6c53d9a6bab69757d0b27409aea05f237e2ff60d
Filipa Martins <fmartins@ubiwhere.com>
2021-12-10 13:20:52 +0000

    Update ip addresses for 5tonic testbed

--- src/environments/5tonic-operatorC.js
+++ src/environments/5tonic-operatorC.js
@@ -1,8 +1,8 @@
 export const API_MARKET_PLACE = 'http://10.4.2.126:31102/tmf-api'
-export const API_LPT = 'http://172.28.3.111/operatora/legal-prose-repository'
-export const API_GOVERNANCE = 'http://172.28.3.111/operatora/governance'
+export const API_LPT = 'http://10.4.2.53:31086/operatora/legal-prose-repository'
+export const API_GOVERNANCE = 'http://10.4.2.53:31084/operatora/governance'
 export const API_IPM = 'http://10.4.2.169:6600'
-export const API_SMART_CONTRACT = 'http://172.28.3.111/operatorc/smart-contract-lifecycle-manager/'
+export const API_SMART_CONTRACT = 'http://10.4.2.53:31082/smart-contract-lifecycle-manager/'
 
 export const XRM_ENDPOINT = 'http://10.4.2.240:31081'
 export const XRM_DISCOVERY_API_KEY = '2e0a0ba5-46b5-41a5-a7c9-3218baf566b8'
