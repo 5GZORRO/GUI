c1cc924f8c72066a74bef776c12d32feb995bf46
Casti <hcastilho8@gmail.com>
2021-12-16 16:09:11 +0000

    advanced search and issm componentes added

--- src/environments/operatorB.js
+++ src/environments/operatorB.js
@@ -1,13 +1,13 @@
 export const API_MARKET_PLACE = 'http://172.28.3.126:32080/tmf-api'
-export const API_LPT = 'http://172.28.3.6:31086'
-export const API_GOVERNANCE = 'http://172.28.3.6:31084'
+export const API_LPT = 'http://172.28.3.111/operatora/legal-prose-repository'
+export const API_GOVERNANCE = 'http://172.28.3.111/operatora/governance'
 export const API_IPM = 'http://172.28.3.153:6800'
-export const API_SMART_CONTRACT = 'http://172.28.3.6:31081/smart-contract-lifecycle-manager/'
+export const API_SMART_CONTRACT = 'http://172.28.3.111/operatorb/smart-contract-lifecycle-manager/'
 
 export const XRM_ENDPOINT = 'http://172.28.3.126:31081'
-export const XRM_DISCOVERY_API_KEY = '7591d6b3-827c-4ac1-a2e0-046c31191910'
-export const XRM_TRANSLATOR_API_KEY = 'f9efe387-90e5-47b8-b795-08bf42c9e974'
-export const RAPP_DISCOVERY_API_KEY = '17069cc2-6c4e-44ac-b9f4-a2a0bbf01be2'
+export const XRM_DISCOVERY_API_KEY = '61a93c48-992d-4f4f-a8c9-2433b090d54b'
+export const XRM_TRANSLATOR_API_KEY = 'bece6398-437e-4a30-9c41-2afc8388e7bd'
+export const RAPP_DISCOVERY_API_KEY = '7fb384dc-12aa-4a23-af93-74c8f7dd2e67'
 
 export const APP_ENV = 'development'
 
