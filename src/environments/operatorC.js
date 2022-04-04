3bbea9e9a4a237e2fe623aeb3986acbf6bbc3872
Filipa Martins <fmartins@ubiwhere.com>
2022-04-04 15:28:48 +0100

    Update API_LPT ports

--- src/environments/operatorC.js
+++ src/environments/operatorC.js
@@ -1,5 +1,5 @@
 export const API_MARKET_PLACE = 'http://172.28.3.126:32180/tmf-api'
-export const API_LPT = 'http://172.28.3.6:31084'
+export const API_LPT = 'http://172.28.3.6:31086'
 export const API_GOVERNANCE = 'http://172.28.3.6:31084'
 export const API_IPM = 'http://172.28.3.153:6600'
 export const API_SMART_CONTRACT = 'http://172.28.3.6:31082/smart-contract-lifecycle-manager/'
