1a121137be55d8224cdd1623078c77be1962b7b8
Filipa Martins <fmartins@ubiwhere.com>
2022-04-26 14:35:07 +0100

    Update ISSM and xRM IPs and keys

--- src/environments/operatorA.js
+++ src/environments/operatorA.js
@@ -4,8 +4,8 @@ export const API_GOVERNANCE = 'http://172.28.3.6:31084'
 export const API_IPM = 'http://172.28.3.153:6200'
 export const API_SMART_CONTRACT = 'http://172.28.3.6:31080/smart-contract-lifecycle-manager/'
 
-export const XRM_ENDPOINT = 'http://172.28.3.126:31081'
-export const XRM_DISCOVERY_API_KEY = '7591d6b3-827c-4ac1-a2e0-046c31191910'
+export const XRM_ENDPOINT = 'http://172.28.3.15:31081'
+export const XRM_DISCOVERY_API_KEY = '2b2bf2cc-46d4-4290-abd4-71de9ba88ba2'
 export const XRM_TRANSLATOR_API_KEY = 'a7622e83-2a7a-4f0c-aacd-3e01fef33c8f'
 export const RAPP_DISCOVERY_API_KEY = '17069cc2-6c4e-44ac-b9f4-a2a0bbf01be2'
 
@@ -17,6 +17,6 @@ export const HOST = 'BCN'
 
 export const API_SRSD = 'http://172.28.3.42:32068'
 
-export const API_ISSM = 'http://172.28.3.42:30080'
+export const API_ISSM = 'http://172.28.3.15:30080'
 
 export const API_RAPP = 'http://172.28.3.242:2626'
